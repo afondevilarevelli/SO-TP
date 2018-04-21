312ad31c0c20230f2a0b5b2005bd52cb6210b795
AntonioDLC <antoniodelascarreras@gmail.com>
2018-04-21 16:35:55 -0300

    Libreria usada para testear conexiones

---
+++ src/shared/testConnection.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "mySocket.h"
+
+void questionWithBasicProtocol(int socket, void * question, int sizeOfQuestion, void *(*showAnswer)(void * answer));
+void answerWithBasicProtocol(int socket, void * answer, int sizeOfAnswer, void *(*showQuestion)(void * question));
