6168914d4e7de9db3ee7a512f658189edbbf2499
Yane801 <yanelayupanqui@gmail.com>
2018-04-23 15:50:08 -0300

    Ajuste en obtener IP

--- src/Planificador/planificador.c
+++ src/Planificador/planificador.c
@@ -25,103 +25,84 @@ void comunicacionESI(){
 	int socket_servidor, new_socket;
 	struct sockaddr_in my_addr, their_addr;
 	int result;
-	void * buffer[256];
+
 
 	socket_servidor = listenOn(inet_addr(IP), PORT);
 
 	new_socket = acceptClient(socket_servidor);
 
-	result = recv(socket_servidor, buffer, sizeof(buffer), 0);
-	if(result == -1){
-		perror("error al recibir datos");
-		exit(1);
-	}
-
-	result = send(socket_servidor, msg, strlen(msg), 0);
-	if(result == -1){
-		perror("error al enviar datos");
-		exit(1);
-	}
+	enviarMensajeESI(socket_servidor);
+	recibirMensajeESI(socket_servidor);
 
 	close(socket_servidor);
 	close(new_socket);
 }
 
-void comunicacionCoord(){
-	int listener, new_socket, fdmax, i,;
-	struct sockaddr_in my_addr;
-	int yes = 1;
-	fd_set master, read_fds;
+int enviarEjecutarProxSentenciaESI(int socket_servidor)
+{
 	int result;
-
-	FD_ZERO(master);
-	FD_ZERO(read_fds);
-
-	if ((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("error al crear el socket");
-		exit(1);
-	}
-
-	if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes,sizeof(int)) == -1) {
-		perror("setsockopt");
-		exit(1);
-	}
-
-	my_addr.sin_family = AF_INET;
-	my_addr.sin_port = htons(PORT);
-	my_addr.sin_addr.s_addr = inet_addr(IP);
-	memset(&(my_addr.sin_zero), 8);
-
-	if ( ( bind(listener, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) )  == -1 ) {
-		perror("error al bindear");
-		exit(1);
-	}
-
-	if ( listen(listener,10) == -1) {
-		perror("error al escuchar");
-	}
-
-	FD_SET(listener, &master);
-	fdmax = listener;
-
-	while(1){
-		read_fds = master;
-		if( (select(fdmax+1, &read_fds, NULL, NULL, NULL)) == -1 ){
-			perror("error en el select");
+   int mensaje =1;
+	result = send(socket_servidor, mensaje, strlen(msg), 0);
+		if(result == -1){
+			perror("error al enviar datos");
 			exit(1);
 		}
+		return result;
+}
+int recibirResultadoDeEjecucionESI(int socket_servidor)
+{
+	int result;
+	void * buffer[256];
+	result = recv(socket_servidor, buffer, sizeof(buffer), 0);
+	if(result ==6){puts("funciono 6");}//Verificacion de q funciona
+		if(result == -1){
+			perror("error al recibir datos");
+			exit(1);
+		}
+	return result;
+}
 
-		for(i = 0; i <= fdmax; i++){
-			if(FD_ISSET(i, read_fds)){
-				if(i == listener){
-					new_socket = accept(i, (struct sockaddr *)&their_addr, sizeof(struct sockaddr_in));
-					FD_SET(new_socket, &master);
-					if(new_socket > fdmax){
-						fdmax = new_socket;
-					}
+
+FD_SET(listener, &master);
+fdmax = listener;
+
+while(1){
+	read_fds = master;
+	if( (select(fdmax+1, &read_fds, NULL, NULL, NULL)) == -1 ){
+		perror("error en el select");
+		exit(1);
+	}
+
+	for(i = 0; i <= fdmax; i++){
+		if(FD_ISSET(i, read_fds)){
+			if(i == listener){
+				new_socket = accept(i, (struct sockaddr *)&their_addr, sizeof(struct sockaddr_in));
+				FD_SET(new_socket, &master);
+				if(new_socket > fdmax){
+					fdmax = new_socket;
+				}
+			}
+			else{
+				void buffer[256];
+				if( ( result = recv(i, buffer, sizeof(buffer), 0) ) == -1){
+					perror("error al recibir datos");
+					exit(1);
+				}
+				if(result == 0){
+					close(i);
 				}
 				else{
-					void buffer[256];
-					if( ( result = recv(i, buffer, sizeof(buffer), 0) ) == -1){
-						perror("error al recibir datos");
-						exit(1);
-					}
-					if(result == 0){
-						close(i);
-					}
-					else{
-						printf("se recibieron %d bytes", result);
-					}
-
-					if( ( result = send(i, msg, strlen(msg), 0) ) == -1 ){
-						perror("error al enviar datos");
-						exit(1);
-					}
-					printf("se enviaron %d bytes", result);
+					printf("se recibieron %d bytes", result);
 				}
+
+				if( ( result = send(i, msg, strlen(msg), 0) ) == -1 ){
+					perror("error al enviar datos");
+					exit(1);
+				}
+				printf("se enviaron %d bytes", result);
 			}
 		}
 	}
-	close(sockfd);
 }
-
+close(sockfd);
+}
