ff29c0cd912b30e603bc17562bdc0de0f5010918
afondevilarevelli <antonny.42@gmail.com>
2018-04-28 17:21:57 -0300

    agrego makefile planif

---
+++ src/shared/algoritmosPlanif.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/collections/queue.h>
+
+#define alpha 40;
+
+t_queue algoritmoSJF(t_queue colaEsi);
+
+t_queue algoritmoSRT(t_queue colaESI);
+
+t_queue algoritmoHRRN(t_queue colaESI);
+
+// devuelven la cola ordenada mediante su algoritmo
