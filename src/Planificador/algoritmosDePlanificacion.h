35bd675928cdf1369659b9f89aa7db6e8f0a3dcf
afondevilarevelli <antonny.42@gmail.com>
2018-05-04 12:37:32 -0300

    header algoritmosDePlanificacion

---
+++ src/Planificador/algoritmosDePlanificacion.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <commons/collections/queue.h>
+
+void ordenarColaSegunFifo(t_queue colaESI);
+
+void ordenarColaSegunSJF(t_queue colaESI);
+
+void ordenarColaSegunSRT(t_queue colaESI);
+
+void ordenarColaSegunHRRN(t_queue colaESI);
