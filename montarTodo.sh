991e11e36f07c931173ffb3a843096fc705382e9
AntonioDLC <antoniodelascarreras@gmail.com>
2018-07-13 01:46:50 -0300

    Bash fix

--- montarTodo.sh
+++ montarTodo.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+cd so-commons-library
+sudo make install
+cd ..
+
 git clone https://github.com/sisoputnfrba/parsi.git
 cd parsi
 sudo make install
@@ -11,5 +16,5 @@ cd src
 
 (cd Coordinador; make clean; make; ../../setIPs coordinador)&
 (cd Planificador; make clean; make; ../../setIPs planificador)&
-(cd ESI; cp -R ../../parsi/src/parsi .; make clean; make; ../../setIPs ESI)&
+(cd ESI; git clone https://github.com/sisoputnfrba/Pruebas-ESI.git; cp -R ../../parsi/src/parsi .; make clean; make; ../../setIPs ESI)&
 (cd Instancia; make clean; make; ../../setIPs instancia)
